dbac700b967c8cb0a3f556393c5e461c9b956080
G VINAY KUMAR <vinaykumarganga19@gmail.com>
2025-05-04 19:30:21 +0530

    Create exp10.py

---
+++ exp10/exp10.py
@@ -0,0 +1,10 @@
+Message digest object info:
+ Algorithm = SHA-1
+ Provider = hashlib (built-in)
+ ToString = hashlib.sha1()
+
+SHA1("") = DA39A3EE5E6B4B0D3255BFEF95601890AFD80709
+
+SHA1("abc") = A9993E364706816ABA3E25717850C26C9CD0D89D
+
+SHA1("abcdefghijklmnopqrstuvwxyz") = 32D10C7B8CF96570CA04CE37F2A19D84240D3A89
